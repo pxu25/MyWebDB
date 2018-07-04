3bcf7aa75a1ccf822ac483600b6db20c0d275b3f
Peng Xu <34561477+pxu25@users.noreply.github.com>
2018-07-04 08:22:47 -0500

    Update MyWeDB.sql

--- MyWeDB.sql
+++ MyWeDB.sql
@@ -69,6 +69,7 @@ VALUES
 (2, 2, GETDATE(),'Sci_Fiction_Movie_LostV',1),
 (3, 2, GETDATE(),'Sci_Fiction_Movie_TransformerIV',2)
 
+-- Extract the custmer email_address, names, download date, file and product				    
 SELECT u.EmailAddress as email_address, u.FirstName as first_name, u.LastName as last_name,
        d.DownloadDate as download_date, d.FileName as filename, p.ProductName as product_name  
 FROM Downloads d Join Users u
@@ -76,3 +77,4 @@ FROM Downloads d Join Users u
 Join Products p
   ON d.ProductID = p.ProductID
 ORDER BY email_address DESC, product_name;
+				  
